5e8aa79efcc5d5c0825a31fda612a7092d4a2b1b
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-05-12 21:41:12 +0200

    refactor(): remove services layer

--- src/heroes/heroes.controller.ts
+++ src/heroes/heroes.controller.ts
@@ -1,19 +1,24 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { HeroesGameService } from './heroes.service';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { KillDragonCommand } from './commands/impl/kill-dragon.command';
 import { KillDragonDto } from './interfaces/kill-dragon-dto.interface';
 import { Hero } from './models/hero.model';
+import { GetHeroesQuery } from './queries/impl';
 
 @Controller('hero')
 export class HeroesGameController {
-  constructor(private readonly heroesService: HeroesGameService) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
 
   @Post(':id/kill')
   async killDragon(@Param('id') id: string, @Body() dto: KillDragonDto) {
-    await this.heroesService.killDragon(id, dto);
+    return this.commandBus.execute(new KillDragonCommand(id, dto.dragonId));
   }
 
   @Get()
   async findAll(): Promise<Hero[]> {
-    return this.heroesService.findAll();
+    return this.queryBus.execute(new GetHeroesQuery());
   }
 }
--- src/heroes/heroes.module.ts
+++ src/heroes/heroes.module.ts
@@ -3,7 +3,6 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { CommandHandlers } from './commands/handlers';
 import { EventHandlers } from './events/handlers';
 import { HeroesGameController } from './heroes.controller';
-import { HeroesGameService } from './heroes.service';
 import { QueryHandlers } from './queries/handlers';
 import { HeroRepository } from './repository/hero.repository';
 import { HeroesGameSagas } from './sagas/heroes.sagas';
@@ -12,7 +11,6 @@ import { HeroesGameSagas } from './sagas/heroes.sagas';
   imports: [CqrsModule],
   controllers: [HeroesGameController],
   providers: [
-    HeroesGameService,
     HeroRepository,
     ...CommandHandlers,
     ...EventHandlers,
