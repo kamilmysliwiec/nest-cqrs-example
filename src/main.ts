94fb6c74b421ebde76d354e086b946da8ca2e0a5
Franz Duran <engrparanz@gmail.com>
2022-09-24 17:33:22 +0800

    add cqrs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Heroes API')
+    .setDescription('CQRS testing API')
+    .setVersion('1.0')
+    // .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/doc', app, document);  
+
   app.listen(3000, () => console.log('Application is listening on port 3000.'));
 }
 bootstrap();
